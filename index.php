e41003bd18cae1e13ed4f3719da29eb89aba9240
Jingjing Zheng <jingjingzheng@g.harvard.edu>
2016-02-16 16:45:13 -0500

    xkcd password generator

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>P2 - xkcd style Password Generator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    </style>
+
+    <?php
+        require 'logic.php';
+    ?>
+
+</head>
+<body>
+    <nav class="navbar navbar-inverse narbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">Home</a>
+            </div>
+                <!-- <ul class="nav navbar-nav">
+                <li class="active"><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul> -->
+        </div>
+    </nav>
+
+    <div class="container-fluid bg-1 text-center">
+        <h1>xkcd Password Generator</h1>
+
+        <p class='password'><?php xkcd_password_generator(); ?></p>
+
+        <form method="get" action="index.php">
+            <p class='options'>
+                <label for='number_of_words'># of Words</label>
+                <input maxlength="1" type='text' name='number_of_words' id='number_of_words' value='<?php $number_of_words ?>'> (Max 9)
+                <br>
+
+                <input type='checkbox' name='add_number' id='add_number' value='Yes'>
+                <label for='add_number'>Add a number</label>
+                <br>
+                <input type='checkbox' name='add_symbol' id='add_symbol' value='Yes'>
+                <label for='add_symbol'>Add a symbol</label>
+            </p>
+
+            <input type='submit' class='btn btn-default' value='Gimme Another'>
+            <div class='error'><?php error_message(); ?></div>
+        </form>
+        <p class='details'>
+            <a href='http://xkcd.com/936/'>xkcd password strength</a><br>
+
+            <a href='http://xkcd.com/936/'>
+                <img class='comic' src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>
+            </a>
+            <br>
+        </p>
+
+    </div>
+</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,108 @@
+<?php
+
+    // Error reporting, and display errors on page
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    /*
+    *   Generate xkcd style password
+    */
+    echo var_dump($_GET);
+    $number_of_words = 0;
+    $errmsg = " ";
+    $add_number = 0;
+    $add_symbol = 0;
+    $gen_pwd    = "";
+    // post validation
+    function validate_options() {
+        global $number_of_words, $add_number, $add_symbol, $errmsg;
+        //number checkbox post-checking
+        if (isset($_GET['add_number']) and ($_GET['add_number'] == 'Yes')) {
+            $add_number = 1;
+            // echo ""$add_number;
+        }
+        echo $add_number;
+
+        //words number post-checking isset
+        $pattern = '/[0-9]/';
+        if (isset($_GET['number_of_words']) and preg_match($pattern, $_GET['number_of_words'])) {
+            $number_of_words = $_GET['number_of_words'];
+            $errmsg = "haha";
+        }
+        else{
+            $number_of_words = 4;
+            $errmsg = "Invalid number entered. Showing a default 4 word long paragraph.";
+            // echo $errmsg;
+        }
+
+        //special character checkbox post-checking
+        if (isset($_GET['add_symbol']) and $_GET['add_symbol'] == 'Yes') {
+            $add_symbol = 1;
+        }
+
+        //words number post-checking
+        // if ($_GET['number_of_words'] == "") {
+        //     global $number_of_words = 4;
+        //     global $errmsg = "Invalid number entered. Showing a default 4 word long paragraph."
+        // }
+        // else {
+        //
+        // }
+    }
+
+    // generate words
+    function generate_wordlist($number_of_words) {
+        $filename = "lib/words.txt";
+        $handle   = fopen($filename, "r");
+        $contents = fread($handle, filesize($filename));
+        fclose($handle);
+        $results = "";
+        $words    = preg_split('/[\s\n]+/', $contents);
+        $length   = sizeof($words);
+        for($i = 0; $i < $number_of_words; $i++) {
+            $results .= $words[rand(0, $length)];
+            $results .= " ";
+        }
+        return $results;
+        // echo $contents;
+    }
+
+    // generate special character
+    function generate_special_character() {
+        $special_char = "{}[];:,./<>?_+~!@#";
+        echo $special_char[rand(0, strlen($special_char))];
+        return $special_char[rand(0, strlen($special_char))];
+    }
+
+    // generate numbers
+    function generate_number() {
+        $numbers = "1234567890";
+        // echo mt_rand(0, 9);
+        return mt_rand(0, 9);
+    }
+
+    // aggregate xkcd password strings
+    function xkcd_password_generator() {
+        global $number_of_words, $add_number, $add_symbol, $errmsg, $gen_pwd;
+        // $gen_pwd = "";
+        validate_options();
+        // echo $number_of_words;
+        $gen_pwd = generate_wordlist($number_of_words);
+        if ($add_number == 1) {
+            $gen_num = generate_number();
+            $gen_pwd = $gen_pwd . strval($gen_num);
+        }
+
+        if ($add_symbol == 1) {
+            $gen_pwd .= generate_special_character();
+        }
+
+        echo $gen_pwd;
+        // echo $errmsg;
+    }
+
+    function error_message() {
+        global $errmsg;
+        echo $errmsg;
+    }
+ ?>
